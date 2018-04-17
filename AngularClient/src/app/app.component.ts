594d657c17484e50a1995d0900ef4eef4558eff7
daniere <dannyreuben5@gmail.com>
2018-04-17 11:12:00 +0300

    Commit

---
+++ AngularClient/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+import { Customer } from './customer';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+}
